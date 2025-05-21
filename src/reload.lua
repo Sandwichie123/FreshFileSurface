971f2180130e0843122ea9a27e08f79f5562e7ff
Sandwichie123 <152046549+Sandwichie123@users.noreply.github.com>
2025-05-21 10:56:03 -0400

    Update reload.lua

--- src/reload.lua
+++ src/reload.lua
@@ -1,4 +1,4 @@
-function StartNewGame_wrap( mapName )
+function StartNewGame_wrap( mapName, args )
 	if currentRun == nil then
 		StartNewRun( nil, { RoomName = "N_Opening01", StartingBiome = "N" } )
 	end
