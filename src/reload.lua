c4dfc6fb7bcc690734faf7877c2c2b62bd176190
Sandwichie123 <152046549+Sandwichie123@users.noreply.github.com>
2025-05-21 23:35:28 -0400

    Update reload.lua

--- src/reload.lua
+++ src/reload.lua
@@ -1,6 +1,20 @@
-function StartNewGame_wrap( mapName, args )
+function StartNewGameWrap( mapName, args )
 	if currentRun == nil then
 		StartNewRun( nil, { RoomName = "N_Opening01", StartingBiome = "N" } )
 	end
 	print("TESTING")
+end
+
+function LoadCurrentRoomResourcesOverride( currentRoom ) 
+	LoadVoiceBanks("MelinoeField", true)
+	local voiceBanks = { currentRoom.Name, currentRoom.ForceLootName, currentRoom.ChosenRewardType, currentRoom.Encounter.Name, currentRoom.Encounter.LootAName, currentRoom.Encounter.LootBName }
+	for _, name in pairs(voiceBanks) do
+		LoadVoiceBanks( GetSpeakerName( name ) )
+	end
+	local packages = { currentRoom.ForceLootName, currentRoom.Encounter.LootAName, currentRoom.Encounter.LootBName, currentRoom.ChosenRewardType }
+	for _, name in pairs(packages) do
+		if not GameData.MissingPackages[lootName] then
+			LoadPackages({ Name = name })
+		end
+	end
 end
