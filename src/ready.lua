5a2127cde67ad102e6f2cd3a8f2ef86ed072b66a
Sandwichie123 <152046549+Sandwichie123@users.noreply.github.com>
2025-05-21 23:35:43 -0400

    Update ready.lua

--- src/ready.lua
+++ src/ready.lua
@@ -1,5 +1,9 @@
 modutil.mod.Path.Wrap("StartNewGame", function( baseFunc, mapName, args )
 	local ret = baseFunc(mapName, args)
-	StartNewGame_wrap(mapName, args)
+	StartNewGameWrap(mapName, args)
 	return ret
+end)
+
+modutil.mod.Path.Override("LoadCurrentRoomResources", function( currentRoom )
+	return LoadCurrentRoomResourcesOverride( currentRoom )
 end)
