f396789e31e1bb1f227d2bd8ef661d3e0129db74
Sandwichie123 <152046549+Sandwichie123@users.noreply.github.com>
2025-05-23 01:21:52 -0400

    Update reload.lua

--- src/reload.lua
+++ src/reload.lua
@@ -1,15 +1,3 @@
-function StartNewGameOverride( mapName, args )
-	DebugAssert({ Condition = GameState == nil, "Overwriting existing game state!" })
-
-	GameState = {}
-	GameStateInit()
-	
-	InitializeMetaUpgradeState()
-	InitializeGiftData()
-	if currentRun == nil then
-		StartNewRun( nil, { RoomName = "N_Opening01", StartingBiome = "N" } )
-	end
-end
 function LoadCurrentRoomResourcesOverride( currentRoom ) 
 	LoadVoiceBanks("MelinoeField", true)
 	local voiceBanks = { currentRoom.Name, currentRoom.ForceLootName, currentRoom.ChosenRewardType, currentRoom.Encounter.Name, currentRoom.Encounter.LootAName, currentRoom.Encounter.LootBName }
