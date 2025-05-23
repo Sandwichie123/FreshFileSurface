f1dafa268939d61c058763bb50465f511cac11dc
Sandwichie123 <152046549+Sandwichie123@users.noreply.github.com>
2025-05-23 01:22:21 -0400

    Update ready.lua

--- src/ready.lua
+++ src/ready.lua
@@ -1,5 +1,5 @@
-modutil.mod.Path.Override("StartNewGame", function( mapName )
-	return StartNewGameOverride(mapName)
+modutil.mod.Path.Wrap("StartNewGame", function( baseFunc, mapName, args )
+	return baseFunc("N_Opening01", args)
 end)
 
 modutil.mod.Path.Override("LoadCurrentRoomResources", function( currentRoom )
